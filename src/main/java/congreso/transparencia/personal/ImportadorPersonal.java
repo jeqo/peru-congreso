ff6b3168ccec1c1cbbef3f28c7132d17f0e48069
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2020-11-14 19:48:53 +0000

    fix comas

--- src/main/java/congreso/transparencia/personal/ImportadorPersonal.java
+++ src/main/java/congreso/transparencia/personal/ImportadorPersonal.java
@@ -1,6 +1,7 @@
 package congreso.transparencia.personal;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Collectors;
@@ -9,18 +10,27 @@ import org.jsoup.Jsoup;
 public class ImportadorPersonal {
 
   public static void main(String[] args) throws IOException {
-    var html = Files.readString(Path.of("static/transparencia/personal/download.html"));
+    var path = Path.of("static/transparencia/personal/download.html");
+    var html = Files.readString(path, Charset.defaultCharset());
     var doc = Jsoup.parse(html);
     var rows = doc.select("tr");
     var headers = rows.first().select("td").stream()
         .map(h -> h.text().trim().toUpperCase())
+        .map(n -> n
+            .replace("PK_", "")
+            .replace("VC_PERSONAL_", "")
+            .replace("MO_PERSONAL_", "")
+            .replace("IN_PERSONAL", ""))
         .collect(Collectors.toList());
+    var cols = headers.size();
+    System.out.println("Cols:" + cols);
     var buffer = new StringBuilder();
     var csvHeader = String.join(",", headers);
     buffer.append(csvHeader).append("\n");
-    for(int i = 1; i < rows.size(); i++) {
+    for (int i = 1; i < rows.size(); i++) {
       var row = rows.get(i).select("td").stream()
-          .map(h -> h.text().trim().toUpperCase())
+          .map(h -> h.text().trim().toUpperCase()
+              .replace(",", ""))
           .collect(Collectors.toList());
       var csvRow = String.join(",", row);
       buffer.append(csvRow).append("\n");
