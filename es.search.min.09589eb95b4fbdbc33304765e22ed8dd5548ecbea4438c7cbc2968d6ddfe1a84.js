bb0c541b412dfdaeca6dc0975eaa8f2a58959698
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2020-12-05 09:01:40 +0000

    publicar

---
+++ es.search.min.09589eb95b4fbdbc33304765e22ed8dd5548ecbea4438c7cbc2968d6ddfe1a84.js
@@ -0,0 +1,11 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
+input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
+const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
+input.focus();event.preventDefault();}
+function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/peru-congreso/flexsearch.min.js');loadScript('/peru-congreso/es.search-data.min.16d84cb8a75db30aa599341c62e3068e976509fb4e0021f38e152a7a34e207b4.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}
+function element(content){const div=document.createElement('div');div.innerHTML=content;return div.firstChild;}})();
